89422206dff1f1daacfd9b48c9eca74ad6f13875
Edd Barrett <vext01@gmail.com>
2019-04-23 17:42:51 +0100

    Use a ykrustc snapshot.

--- .buildbot.sh
+++ .buildbot.sh
@@ -2,41 +2,9 @@
 
 set -e
 
-# First we have to build ykrustc, complete with cargo and rustfmt.
-ykrustc_prefix=`pwd`/ykrustc-inst
-ykrustc_version=master
-
-if ! [ -d ykrustc ]; then
-    git clone https://github.com/softdevteam/ykrustc
-fi
-cd ykrustc
-git checkout ${ykrustc_version}
-
-cat << EOF > config.toml
-[build]
-extended = true
-tools = ["cargo", "rustfmt"]
-
-[install]
-prefix = "${ykrustc_prefix}"
-sysconfdir = "etc"
-
-[rust]
-codegen-units = 0
-debug-assertions = true
-
-[llvm]
-assertions = true
-EOF
-
-mkdir -p ${ykrustc_prefix}
-
-if ! [ -e "${ykrustc_prefix}/bin/rustc" ]; then
-    ./x.py install
-fi
-
-export PATH=${ykrustc_prefix}/bin:${PATH}
-cd ..
+# Use the most recent successful ykrustc build.
+tar jxf /opt/ykrustc-bin-snapshots/ykrustc-stage2-latest.tar.bz2
+export PATH=ykrustc-stage2/bin:${PATH}
 
 cargo fmt --all -- --check
 cargo test
