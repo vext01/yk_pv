c7dabd3c93a769ee0814521e51f2b5e8a50fa5e6
Edd Barrett <vext01@gmail.com>
2021-01-25 15:27:47 +0000

    Prefix test API shims with __ykshimtest_.
    This way we can easily see if we erroneously use a part of the testing
    API in the production part of the system.
    
    No functional change.

--- internal_ws/ykshim/src/test_api.rs
+++ internal_ws/ykshim/src/test_api.rs
@@ -13,13 +13,13 @@ use yktrace::tir::TirTrace;
 
 /// Returns the length of a SIR trace.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_sirtrace_len(sir_trace: *mut SirTrace) -> size_t {
+unsafe extern "C" fn __ykshimtest_sirtrace_len(sir_trace: *mut SirTrace) -> size_t {
     (&mut *sir_trace).len()
 }
 
 /// Compile a TIR trace from a SIR trace.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tirtrace_new<'a, 'm>(
+unsafe extern "C" fn __ykshimtest_tirtrace_new<'a, 'm>(
     sir_trace: *mut SirTrace,
 ) -> *mut TirTrace<'a, 'm> {
     let sir_trace = &mut *(sir_trace);
@@ -28,13 +28,13 @@ unsafe extern "C" fn __ykshim_tirtrace_new<'a, 'm>(
 
 /// Returns the length of a SIR trace.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tirtrace_len<'a, 'm>(tir_trace: *mut TirTrace<'a, 'm>) -> size_t {
+unsafe extern "C" fn __ykshimtest_tirtrace_len<'a, 'm>(tir_trace: *mut TirTrace<'a, 'm>) -> size_t {
     Box::from_raw(tir_trace).len()
 }
 
 /// Returns the human-readable Display string of a TIR trace.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tirtrace_display<'a, 'm>(
+unsafe extern "C" fn __ykshimtest_tirtrace_display<'a, 'm>(
     tir_trace: *mut TirTrace<'a, 'm>,
 ) -> *mut c_char {
     let tt = Box::from_raw(tir_trace);
@@ -45,7 +45,7 @@ unsafe extern "C" fn __ykshim_tirtrace_display<'a, 'm>(
 /// Looks up the TypeId of the return value of the given symbol. The TypeId is returned in two
 /// parts in `ret_cgu` and `ret_idx`.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_body_ret_ty(
+unsafe extern "C" fn __ykshimtest_body_ret_ty(
     sym: *mut c_char,
     ret_cgu: *mut CguHash,
     ret_idx: *mut TyIndex,
@@ -59,21 +59,21 @@ unsafe extern "C" fn __ykshim_body_ret_ty(
 
 /// Creates a TraceCompiler with default settings.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tracecompiler_default() -> *mut TraceCompiler {
+unsafe extern "C" fn __ykshimtest_tracecompiler_default() -> *mut TraceCompiler {
     let tc = Box::new(TraceCompiler::new(Default::default(), Default::default()));
     Box::into_raw(tc)
 }
 
 /// Drop a TraceCompiler.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tracecompiler_drop(comp: *mut c_void) {
+unsafe extern "C" fn __ykshimtest_tracecompiler_drop(comp: *mut c_void) {
     Box::from_raw(comp as *mut TraceCompiler);
 }
 
 /// Inserts a local declaration of the specified TypeId into a TraceCompiler. The TypeId is passed
 /// in two parts: a CGU hash and a type index.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tracecompiler_insert_decl(
+unsafe extern "C" fn __ykshimtest_tracecompiler_insert_decl(
     tc: *mut c_void,
     local: Local,
     local_ty_cgu: CguHash,
@@ -92,7 +92,7 @@ unsafe extern "C" fn __ykshim_tracecompiler_insert_decl(
 
 /// Returns a string describing the register allocation of the specified local.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tracecompiler_local_to_location_str(
+unsafe extern "C" fn __ykshimtest_tracecompiler_local_to_location_str(
     tc: *mut c_void,
     local: Local,
 ) -> *mut c_char {
@@ -103,14 +103,14 @@ unsafe extern "C" fn __ykshim_tracecompiler_local_to_location_str(
 
 /// Inform a TraceCompiler's register allocator that a local variable is dead.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tracecompiler_local_dead(tc: *mut TraceCompiler, local: Local) {
+unsafe extern "C" fn __ykshimtest_tracecompiler_local_dead(tc: *mut TraceCompiler, local: Local) {
     let tc = &mut *tc;
     tc.local_dead(&local).unwrap();
 }
 
 /// Find a symbol's address in the current memory image. Returns NULL if it can't be found.
 #[no_mangle]
-unsafe extern "C" fn __ykshim_tracecompiler_find_sym(sym: *mut c_char) -> *mut c_void {
+unsafe extern "C" fn __ykshimtest_tracecompiler_find_sym(sym: *mut c_char) -> *mut c_void {
     TraceCompiler::find_symbol(CString::from_raw(sym).to_str().unwrap())
         .unwrap_or_else(|_| ptr::null_mut())
 }
